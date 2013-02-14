ead122d236cddfa1b9f95fe9dc0eca59df0026ca
R00bert <osiadaczrobert@gmail.com>
2013-02-14 14:34:58 +0100

    Main commit

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -19,9 +19,11 @@ class Main extends Controller {
         $this->include_controller('hub');
         $this->include_controller('home');
         $this->include_controller('article');
-        //
+        // category
         $this->smarty->assign('category', $this->category_load_all());
+        // vendor
         $this->smarty->assign('vendor', $this->vendor_load_all());
+
 	}
 
 	// index
